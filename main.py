3da554eabe65a40c7311f0b210db0785ca61f9c6
Jose <jakermater@gmail.com>
2023-01-23 14:38:25 +0100

    always lying, end of the logic

---
+++ main.py
@@ -0,0 +1,5 @@
+def is_politician_lying(name='', party=''):
+    return 'Is lying and stealing'
+
+def esta_mintiendo_el_politico(name='', party=''):
+    return 'Está mintiendo y robando a manos llenas'
